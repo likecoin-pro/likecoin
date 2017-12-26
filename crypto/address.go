f2285aba76cf5053a13cd3d39e066f89360ec8bf
denisskin <denis@begom.com>
2017-12-27 05:23:16 +0800

    add address

---
+++ crypto/address.go
@@ -0,0 +1,88 @@
+package crypto
+
+import (
+	"bytes"
+	"errors"
+	"strings"
+
+	"github.com/denisskin/bin"
+	"github.com/likecoin-pro/likecoin/crypto/base58"
+)
+
+const (
+	AddressSize = 24
+
+	addressPrefix = "Like"
+	addressVer    = '\xf3'
+	checksumLen   = 3
+)
+
+type Address [AddressSize]byte
+
+var (
+	errAddrInvalidLength        = errors.New("crypto.newAddress: Invalid address length")
+	errParseAddrInvalid         = errors.New("crypto.ParseAddress: Invalid address")
+	errParseAddrUnknownVer      = errors.New("crypto.ParseAddress: Unknown address version")
+	errParseAddrInvalidCheckSum = errors.New("crypto.ParseAddress: Invalid check-sum")
+)
+
+func newAddress(data []byte) (addr Address) {
+	if len(data) != AddressSize {
+		panic(errAddrInvalidLength)
+	}
+	copy(addr[:], data[:AddressSize])
+	return
+}
+
+func (addr Address) String() string {
+	return addr.Encode(0)
+}
+
+func addrCheckSum(addr []byte, mgNum int64) []byte {
+	h := newHash256()
+	h.Write([]byte(addressPrefix))
+	h.Write([]byte{addressVer})
+	h.Write(addr)
+	h.Write(bin.Uint64ToBytes(uint64(mgNum)))
+	return hash256(h.Sum(nil))[:checksumLen]
+}
+
+func (addr Address) Encode(magicNum int64) string {
+	w := bin.NewBuffer(nil)
+	w.WriteByte(addressVer) // first byte have to > 0
+	w.Write(addr[:])
+	w.WriteVarInt64(magicNum)
+	w.Write(addrCheckSum(addr[:], magicNum))
+	return addressPrefix + base58.Encode(w.Bytes())
+}
+
+func ParseAddress(strAddr string) (addr Address, magicNum int64, err error) {
+	if !strings.HasPrefix(strAddr, addressPrefix) {
+		err = errParseAddrUnknownVer
+		return
+	}
+	bb, err := base58.Decode(strAddr[len(addressPrefix):])
+	if err != nil {
+		return
+	}
+	r := bin.NewBuffer(bb)
+	if b, e := r.ReadByte(); b != addressVer || e != nil {
+		err = errParseAddrInvalid
+		return
+	}
+	if _, err = r.Read(addr[:]); err != nil {
+		err = errParseAddrInvalid
+		return
+	}
+	if magicNum, err = r.ReadVarInt64(); err != nil {
+		err = errParseAddrInvalid
+		return
+	}
+	chSum := make([]byte, checksumLen)
+	_, err = r.Read(chSum[:])
+	if err != nil || !bytes.Equal(chSum, addrCheckSum(addr[:], magicNum)) {
+		err = errParseAddrInvalidCheckSum
+		return
+	}
+	return
+}
