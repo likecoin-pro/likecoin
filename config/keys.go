50eabf650e0833fd6fc3568ea62b533ad9d45ec1
denisskin <denis@begom.com>
2018-02-05 20:13:32 +0300

    add master keys

---
+++ config/keys.go
@@ -0,0 +1,8 @@
+package config
+
+import "github.com/likecoin-pro/likecoin/crypto"
+
+var (
+	MasterPublicKey   = crypto.MustParsePublicKey("4E2C3rgLegpqYPozX8z7WYi27Govf1a4P9ukQ8B4MR8EnmT6fSDk8Tu5DhZfas94qzXbkzmswwZuWPANJrY4G67s")
+	EmissionPublicKey = crypto.MustParsePublicKey("4SC2HYTGqUVSn1kJENxB6yDZXn8HARbbzWDtVnMyrx4zzWYNFJcaqhzerztXmXpMk5wH2Jkt7RbenEdhoHHaYDHp")
+)
