33a474d68be2791c9a04db18fe3a35a32718f906
denis <denis@begom.com>
2018-03-11 11:25:38 +0300

    hash to crypto; use sha3

--- crypto/hash.go
+++ crypto/hash.go
@@ -1,15 +1,32 @@
 package crypto
 
 import (
-	"crypto/sha256"
 	"hash"
+
+	"github.com/denisskin/bin"
+	"golang.org/x/crypto/sha3"
 )
 
 func hash256(data []byte) []byte {
-	h := sha256.Sum256(data)
+	h := sha3.Sum256(data)
 	return h[:]
 }
 
 func newHash256() hash.Hash {
-	return sha256.New()
+	return sha3.New256()
+}
+
+func Hash256(values ...interface{}) []byte {
+	if len(values) == 1 {
+		if bb, ok := values[0].([]byte); ok {
+			return hash256(bb)
+		}
+	}
+
+	h := newHash256()
+	w := bin.NewWriter(h)
+	for _, val := range values {
+		w.WriteVar(val)
+	}
+	return h.Sum(nil)
 }
