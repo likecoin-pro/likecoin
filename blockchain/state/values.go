13d1be3a589ed32956ce1246fda86979601c5424
denis <denis@begom.com>
2018-04-19 06:25:17 +0300

    stringify

--- blockchain/state/values.go
+++ blockchain/state/values.go
@@ -4,6 +4,14 @@ import "github.com/likecoin-pro/likecoin/crypto/merkle"
 
 type Values []*Value
 
+func (vv Values) String() string {
+	s := "[\n"
+	for _, v := range vv {
+		s += "  " + v.String() + ",\n"
+	}
+	return s + "]"
+}
+
 func (vv Values) Equal(b Values) bool {
 	if len(vv) != len(b) {
 		return false
