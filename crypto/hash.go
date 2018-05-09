31a44702e2a5540afc32ac1f822ecfae1309c2b1
denis <denis@begom.com>
2018-05-09 16:29:48 +0300

    use Keccak sha3

--- crypto/hash.go
+++ crypto/hash.go
@@ -4,20 +4,17 @@ import (
 	"hash"
 
 	"github.com/denisskin/bin"
-	"golang.org/x/crypto/sha3"
+	"github.com/ethereum/go-ethereum/crypto/sha3"
 )
 
-func hash256(data []byte) []byte {
-	h := sha3.Sum256(data)
-	return h[:]
-}
-
 func newHash256() hash.Hash {
-	return sha3.New256()
+	return sha3.NewKeccak256()
 }
 
-func Hash256Raw(data []byte) []byte {
-	return hash256(data)
+func HashSum256(data []byte) []byte {
+	sha := newHash256()
+	sha.Write(data)
+	return sha.Sum(nil)
 }
 
 func Hash256(values ...interface{}) []byte {
