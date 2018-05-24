2d5775bc7228821c9485565075d41d90a879bd95
denis <denis@begom.com>
2018-05-24 11:42:27 +0300

    use shake256

--- crypto/hash.go
+++ crypto/hash.go
@@ -1,27 +1,25 @@
 package crypto
 
 import (
-	"hash"
-
 	"github.com/denisskin/bin"
 	"github.com/ethereum/go-ethereum/crypto/sha3"
 )
 
-func newHash256() hash.Hash {
-	return sha3.NewKeccak256()
-}
-
 func HashSum256(data []byte) []byte {
-	sha := newHash256()
+	var hash [32]byte
+	sha := sha3.NewShake256()
 	sha.Write(data)
-	return sha.Sum(nil)
+	sha.Read(hash[:])
+	return hash[:]
 }
 
 func Hash256(values ...interface{}) []byte {
-	h := newHash256()
-	w := bin.NewWriter(h)
+	var hash [32]byte
+	sha := sha3.NewShake256()
+	w := bin.NewWriter(sha)
 	for _, val := range values {
 		w.WriteVar(val)
 	}
-	return h.Sum(nil)
+	sha.Read(hash[:])
+	return hash[:]
 }
