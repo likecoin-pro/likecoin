7469a87d726e026385538ec765097d3a7472a688
denis <denis@begom.com>
2018-06-04 06:38:22 +0300

    add tests

---
+++ blockchain/bc_test.go
@@ -0,0 +1,41 @@
+package blockchain
+
+import (
+	"errors"
+
+	"github.com/denisskin/bin"
+	"github.com/likecoin-pro/likecoin/assets"
+	"github.com/likecoin-pro/likecoin/blockchain/state"
+)
+
+var TestCounter = assets.Asset{1}
+
+type TestTxObject struct {
+	Transaction
+	Msg string
+}
+
+var _ = RegisterTxObject(127, &TestTxObject{})
+
+func (obj *TestTxObject) Encode() []byte {
+	return bin.Encode(
+		obj.Msg,
+	)
+}
+
+func (obj *TestTxObject) Decode(data []byte) error {
+	return bin.Decode(data,
+		&obj.Msg,
+	)
+}
+
+func (obj *TestTxObject) Verify(tx *Transaction) error {
+	if len(obj.Msg) > 100 {
+		return errors.New("tx.msg is too long")
+	}
+	return nil
+}
+
+func (obj *TestTxObject) Execute(tx *Transaction, st *state.State) {
+	st.Increment(TestCounter, obj.Sender.Address(), state.Int(int64(len(obj.Msg))), 0)
+}
