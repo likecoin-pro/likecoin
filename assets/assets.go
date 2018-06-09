01debb2e5d25037fdd3053ca7b03bf7a0406b1d0
denis <denis@begom.com>
2018-06-09 11:30:31 +0300

    refactor assets

--- assets/assets.go
+++ assets/assets.go
@@ -8,12 +8,9 @@ const (
 )
 
 var (
-	LikeCoin = YotubeCoin
+	Likecoin = Asset{CoinType, 1} // is synonym of "YotubeCoin"
 
-	// coins
-	YotubeCoin = newCoin(1, "LIKE", "https://www.youtube.com/watch?v={ID}", 10*Coin, 2e-7)
-	//InstaCoin  = newCoin(2, "INSTA","https://www.instagram.com/p/{ID}/", 0,0)
-	//YoukuCoin  = newCoin(3,"YOUKU","https://v.youku.com/v_show/id_{ID}", 0,0)
+	Default = Likecoin
 )
 
 func NewCounter(typ uint8, id string) Asset {
