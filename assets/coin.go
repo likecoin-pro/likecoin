ff6caea35f743cbcc3dbabfd760c3de45ea41d94
denis <denis@begom.com>
2018-06-09 12:31:12 +0300

    refactor assets

--- assets/coin.go
+++ assets/coin.go
@@ -7,22 +7,3 @@ const (
 	MicroCoin = Coin / 1e6
 	NanoCoin  = Coin / 1e9
 )
-
-type coinConfig struct {
-	Label     string
-	SrcURL    string
-	EmissionA float64
-	EmissionK float64
-}
-
-var coinsCFG = map[uint8]*coinConfig{}
-
-func newCoin(id uint8, label, srcURL string, emissionA int64, emissionK float64) Asset {
-	coinsCFG[id] = &coinConfig{
-		Label:     label,
-		SrcURL:    srcURL,
-		EmissionA: float64(emissionA),
-		EmissionK: emissionK,
-	}
-	return Asset{CoinType, id}
-}
