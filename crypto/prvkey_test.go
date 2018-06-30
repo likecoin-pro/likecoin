83c75ebad5270524c3754b9e1e2c229c055f803c
denis <denis@begom.com>
2018-07-01 02:38:23 +0300

    fix test

--- crypto/prvkey_test.go
+++ crypto/prvkey_test.go
@@ -19,11 +19,24 @@ func TestPrivateKey_String(t *testing.T) {
 
 func TestParsePrivateKey(t *testing.T) {
 	prv, err := ParsePrivateKey("0x016c23f9a5fc3609c74a37e2fb7982653c97e39f00a8f700f99e3b770bb872bd8c")
-	pub := prv.PublicKey
 
 	assert.NoError(t, err)
 	assert.Equal(t, "0x016c23f9a5fc3609c74a37e2fb7982653c97e39f00a8f700f99e3b770bb872bd8c", prv.Hex())
-	assert.Equal(t, "0xcca27aa571d2838209895c8151cff2ade07e56c0c62a64cd3c84ad73a4287141b207d20a99a5c3169f5f086c3bd05480cad1ad1359a7f01151ed2fd9b2c67601", pub.Hex())
+	assert.Equal(t, "0xcca27aa571d2838209895c8151cff2ade07e56c0c62a64cd3c84ad73a4287141b207d20a99a5c3169f5f086c3bd05480cad1ad1359a7f01151ed2fd9b2c67601", prv.PublicKey.Hex())
+}
+
+func TestParsePrivateKey_oldFormat(t *testing.T) {
+	prv, err := ParsePrivateKey("0x6c23f9a5fc3609c74a37e2fb7982653c97e39f00a8f700f99e3b770bb872bd8c")
+
+	assert.NoError(t, err)
+	assert.Equal(t, "0x016c23f9a5fc3609c74a37e2fb7982653c97e39f00a8f700f99e3b770bb872bd8c", prv.Hex())
+	assert.Equal(t, "0xcca27aa571d2838209895c8151cff2ade07e56c0c62a64cd3c84ad73a4287141b207d20a99a5c3169f5f086c3bd05480cad1ad1359a7f01151ed2fd9b2c67601", prv.PublicKey.Hex())
+}
+
+func TestParsePrivateKey_fail(t *testing.T) {
+	_, err := ParsePrivateKey("0x026c23f9a5fc3609c74a37e2fb7982653c97e39f00a8f700f99e3b770bb872bd8c")
+
+	assert.Error(t, err)
 }
 
 func TestGeneratePrivateKey(t *testing.T) {
