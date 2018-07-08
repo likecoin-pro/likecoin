7708de6ca6daf22f6a2795f1b442e4ba8c1b2b9c
denis <denis@begom.com>
2018-07-08 04:41:18 +0300

    fix genesis

--- blockchain/genesis.go
+++ blockchain/genesis.go
@@ -2,19 +2,16 @@ package blockchain
 
 import "github.com/likecoin-pro/likecoin/config"
 
-var genesisBlock = &Block{
-	Version:   0,
-	Num:       0,
-	ChainID:   1,
-	Timestamp: 0,
-	PrevHash:  nil,
-	TxRoot:    nil,
-	Nonce:     0,
-	Miner:     config.MasterPublicKey,
-}
-
-var genesisBlockHash = genesisBlock.Hash()
-
 func GenesisBlock() *Block {
-	return genesisBlock
+	return &Block{
+		Version:   0,
+		Num:       0,
+		ChainID:   config.ChainID,
+		Network:   config.NetworkID,
+		Timestamp: 0,
+		PrevHash:  nil,
+		TxRoot:    nil,
+		Nonce:     0,
+		Miner:     config.MasterPublicKey,
+	}
 }
