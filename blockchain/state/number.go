c66a093139cd8c737649f768646a94e58bf9d06d
denis <denis@begom.com>
2018-07-09 14:13:27 +0300

    refactor state number

--- blockchain/state/number.go
+++ blockchain/state/number.go
@@ -1,33 +1,9 @@
 package state
 
-import (
-	"fmt"
-	"math/big"
-	"strings"
-
-	"github.com/likecoin-pro/likecoin/assets"
-)
+import "math/big"
 
 type Number = *big.Int
 
 func Int(v int64) Number {
 	return big.NewInt(v)
-}
-
-func StrAmount(amount interface{}, a assets.Asset) string {
-	if len(a) == 0 {
-		a = assets.Default
-	}
-	s := fmt.Sprint(amount)
-	if a.IsCoin() {
-		_s := "000000000" + s
-		s, _s = strings.TrimLeft(_s[:len(_s)-9], "0"), strings.TrimRight(_s[len(_s)-9:], "0")
-		if s == "" {
-			s = "0"
-		}
-		if _s != "" {
-			s += "." + _s
-		}
-	}
-	return s
 }
