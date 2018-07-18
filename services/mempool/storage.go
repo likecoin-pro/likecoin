56b760d9fab7db70a23400a5ea7fc836c9f72754
denis <denis@begom.com>
2018-07-18 12:35:55 +0300

    fix storage

--- services/mempool/storage.go
+++ services/mempool/storage.go
@@ -4,25 +4,44 @@ import (
 	"sync"
 
 	"github.com/likecoin-pro/likecoin/blockchain"
+	"github.com/likecoin-pro/likecoin/crypto"
 )
 
 type Storage struct {
 	mx   sync.RWMutex
-	vals []*blockchain.BlockTx
+	vals []*blockchain.Transaction
 }
 
 func NewStorage() *Storage {
 	return &Storage{}
 }
 
-func (s *Storage) Put(tx *blockchain.BlockTx) error {
+func (s *Storage) Size() int {
+	s.mx.RLock()
+	defer s.mx.RUnlock()
+	return len(s.vals)
+}
+
+// todo: add counters by txType
+func (s *Storage) SizeOf(txType blockchain.TxType) (count int) {
+	s.mx.RLock()
+	defer s.mx.RUnlock()
+	for _, tx := range s.vals {
+		if tx.Type == txType {
+			count++
+		}
+	}
+	return
+}
+
+func (s *Storage) Put(tx *blockchain.Transaction) error {
 	s.mx.Lock()
 	defer s.mx.Unlock()
 	s.vals = append(s.vals, tx)
 	return nil
 }
 
-func (s *Storage) Pop() (tx *blockchain.BlockTx) {
+func (s *Storage) Pop() (tx *blockchain.Transaction) {
 	s.mx.Lock()
 	defer s.mx.Unlock()
 	if len(s.vals) > 0 {
@@ -32,10 +51,29 @@ func (s *Storage) Pop() (tx *blockchain.BlockTx) {
 	return
 }
 
-func (s *Storage) PopAll() (txs []*blockchain.BlockTx) {
+func (s *Storage) PopAll() (txs []*blockchain.Transaction) {
 	s.mx.Lock()
 	defer s.mx.Unlock()
 	txs = s.vals
 	s.vals = nil
+	return
+}
+
+func (s *Storage) TxsByAddress(addr crypto.Address) (txs []*blockchain.Transaction, err error) {
+	s.mx.RLock()
+	defer s.mx.RUnlock()
+	for _, tx := range s.vals {
+		if tx.SenderAddress() == addr {
+			txs = append(txs, tx)
+		}
+	}
+	return
+}
+
+func (s *Storage) TxsAll() (txs []*blockchain.Transaction, err error) {
+	s.mx.RLock()
+	defer s.mx.RUnlock()
+	txs = make([]*blockchain.Transaction, len(s.vals))
+	copy(txs, s.vals)
 	return
 }
