3837c300b2154daadeaf535d7cfe2eb01f2e1566
denis <denis@begom.com>
2018-09-11 16:17:35 +0300

    add BinarySizeStr

--- commons/enc/string.go
+++ commons/enc/string.go
@@ -3,6 +3,8 @@ package enc
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 // String returns object as string (encode to json)
@@ -33,4 +35,30 @@ func IndentJSON(v interface{}) string {
 		panic(err)
 	}
 	return string(b)
+}
+
+var sizeSfxs = []string{" B", " KB", " MB", " GB", " TB", " PB", " EB"}
+
+func BinarySizeStr(size int64) string {
+	f, sfx, pfx := float64(size), 0, ""
+	if f < 0 {
+		f, pfx = -f, "-"
+	}
+	for f >= 1000 {
+		f /= 1024
+		sfx++
+	}
+	v := strconv.FormatFloat(f, 'f', 3, 64)
+	if len(v) > 4 {
+		v = v[:4]
+	}
+	if strings.IndexByte(v, '.') > 0 {
+		for v[len(v)-1] == '0' { // trim right '0'
+			v = v[:len(v)-1]
+		}
+	}
+	if v[len(v)-1] == '.' { // trim right '.'
+		v = v[:len(v)-1]
+	}
+	return pfx + v + sizeSfxs[sfx]
 }
