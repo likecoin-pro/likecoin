cfd2d09e07385583fbc9a9c8c420077d97f8e68f
denis <denis@begom.com>
2018-12-20 08:34:14 +0300

    fix test

--- blockchain/tx_test.go
+++ blockchain/tx_test.go
@@ -1,29 +1,54 @@
 package blockchain
 
 import (
+	"encoding/hex"
 	"testing"
 
 	"github.com/denisskin/bin"
-	"github.com/likecoin-pro/likecoin/config"
 	"github.com/likecoin-pro/likecoin/crypto"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTransaction_Decode(t *testing.T) {
-	tx1 := &Transaction{
+var (
+	testPrv = crypto.NewPrivateKeyBySecret("abc")
+	testPub = testPrv.PublicKey
+)
+
+func TestTransaction_Encode(t *testing.T) {
+	tx := &Transaction{
 		Type:    1,
 		Version: 0,
-		Network: config.NetworkID,
-		ChainID: config.ChainID,
-		Nonce:   123456789,
-		Sender:  crypto.NewPrivateKey().PublicKey,
-		Data:    []byte("0123456789abcdefg"),
+		Network: NetworkWorking,
+		ChainID: 1,
+		Nonce:   12345,
+		Sender:  testPub,
+		Data:    []byte("abcdefg"),
 	}
-	data := bin.Encode(tx1)
 
-	var tx2 *Transaction
-	err := bin.Decode(data, &tx2)
+	data := bin.Encode(tx)
+
+	assert.Equal(t,
+		"3501000001823039076162636465666700002102022f86f8c408c20e8bdcef6471676a2157624915355fe662b568ac5e2a2a76fe0000",
+		hex.EncodeToString(data),
+	)
+}
+
+func TestTransaction_Decode(t *testing.T) {
+	data, _ := hex.DecodeString(
+		"3501000001823039076162636465666700002102022f86f8c408c20e8bdcef6471676a2157624915355fe662b568ac5e2a2a76fe0000",
+	)
+
+	var tx *Transaction
+	err := bin.Decode(data, &tx)
 
 	assert.NoError(t, err)
-	assert.EqualValues(t, tx1, tx2)
+	assert.EqualValues(t, &Transaction{
+		Type:    1,
+		Version: 0,
+		Network: NetworkWorking,
+		ChainID: 1,
+		Nonce:   12345,
+		Sender:  testPub,
+		Data:    []byte("abcdefg"),
+	}, tx)
 }
