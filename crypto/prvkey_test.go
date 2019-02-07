bd8922a3efdf0c3d1f75e3d288eb5380b0bd74c7
denis <denis@begom.com>
2019-02-07 03:27:14 +0300

    fix test

--- crypto/prvkey_test.go
+++ crypto/prvkey_test.go
@@ -6,13 +6,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+//func Test__(t *testing.T) {
+//	for i := 0; i < 1000; i++ {
+//		p := NewPrivateKey().PublicKey
+//		//println(hex.EncodeToString(p.Encode()), hex.EncodeToString(p.y.Bytes()))
+//		//y := p.y.Bytes()
+//		assert.Equal(t, p.Encode(), p.myEncode())
+//	}
+//}
+
 func TestPrivateKey_String(t *testing.T) {
 	prv := NewPrivateKeyBySecret("abc")
 	pub := prv.PublicKey
 	addr := pub.Address()
 
 	assert.Equal(t, "0x01d85f68a0dd6b2ebeb5a60b47b70d2e4b63a842ac0510116e2d52f153e535cd61", prv.String())
-	assert.Equal(t, "0x022f86f8c408c20e8bdcef6471676a2157624915355fe662b568ac5e2a2a76fed5d34d4a184176a3e4a28bac7203a860510e363601f7c8f8657067173ed83f6e", pub.Hex())
+	assert.Equal(t, "0x02022f86f8c408c20e8bdcef6471676a2157624915355fe662b568ac5e2a2a76fe", pub.String())
 	assert.Equal(t, "0x9a8a9d2b5766b5c3962f4dd301c01765bdc37a6387f24250", addr.Hex())
 	assert.Equal(t, "Like5DuaVTk8KgpRh98xDvHvnpaAWxSoYh6uLRvyar5", addr.String())
 }
@@ -22,7 +31,7 @@ func TestParsePrivateKey(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, "0x016c23f9a5fc3609c74a37e2fb7982653c97e39f00a8f700f99e3b770bb872bd8c", prv.Hex())
-	assert.Equal(t, "0xcca27aa571d2838209895c8151cff2ade07e56c0c62a64cd3c84ad73a4287141b207d20a99a5c3169f5f086c3bd05480cad1ad1359a7f01151ed2fd9b2c67601", prv.PublicKey.Hex())
+	assert.Equal(t, "0x03cca27aa571d2838209895c8151cff2ade07e56c0c62a64cd3c84ad73a4287141", prv.PublicKey.String())
 }
 
 func TestParsePrivateKey_oldFormat(t *testing.T) {
@@ -30,7 +39,7 @@ func TestParsePrivateKey_oldFormat(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, "0x016c23f9a5fc3609c74a37e2fb7982653c97e39f00a8f700f99e3b770bb872bd8c", prv.Hex())
-	assert.Equal(t, "0xcca27aa571d2838209895c8151cff2ade07e56c0c62a64cd3c84ad73a4287141b207d20a99a5c3169f5f086c3bd05480cad1ad1359a7f01151ed2fd9b2c67601", prv.PublicKey.Hex())
+	assert.Equal(t, "0x03cca27aa571d2838209895c8151cff2ade07e56c0c62a64cd3c84ad73a4287141", prv.PublicKey.String())
 }
 
 func TestParsePrivateKey_fail(t *testing.T) {
@@ -63,7 +72,7 @@ func TestGeneratePrivateKeyByPassword(t *testing.T) {
 	pub3 := prv3.PublicKey
 
 	assert.Equal(t, "0x015491dbe82c98002178a5bd37f211c09bea225a9c2b7ccf0f52c4d4ee2db4e123", prv1.String())
-	assert.Equal(t, "mcPnH3mnGzQmAmcWRJRLaQqYNVSKFzvaBUaC1BYKMEXS", pub1.String())
+	assert.Equal(t, "0x0296d34fac2486115cba7fdea208a66a6b1eaea2ffcce09140c1993013fb8255d9", pub1.String())
 	assert.Equal(t, 33, len(prv1.Encode()))
 	assert.Equal(t, prv1.Encode(), prv2.Encode())
 	assert.Equal(t, pub1.Encode(), pub2.Encode())
